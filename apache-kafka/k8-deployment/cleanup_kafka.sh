2b39b3f6ec3dbd203824045c7819cc60dce0ef84
Javier Santos <47392657+javisantossanchez@users.noreply.github.com>
2024-05-29 13:03:33 +0200

    Update cleanup_kafka.sh

--- apache-kafka/k8-deployment/cleanup_kafka.sh
+++ apache-kafka/k8-deployment/cleanup_kafka.sh
@@ -7,26 +7,15 @@ echo "Deleting Kafka and Zookeeper deployments, services, and other resources...
 # Namespaces (assuming they are in the default namespace)
 NAMESPACE="default"
 
-# Delete deployments
-microk8s kubectl delete deployment -n $NAMESPACE kafka1 kafka2 kafka3 kafka4 kafka-ui zookeeper
-
-# Delete services
-microk8s kubectl delete service -n $NAMESPACE kafka1 kafka2 kafka3 kafka4 kafka-ui zookeeper
-
 # Delete pods
-microk8s kubectl delete pod -n $NAMESPACE -l app=kafka
-microk8s kubectl delete pod -n $NAMESPACE -l app=zookeeper
-microk8s kubectl delete pod -n $NAMESPACE -l app=kafka-ui
+microk8s kubectl delete pod -n $NAMESPACE
 
 # Delete persistent volume claims (PVCs)
-microk8s kubectl delete pvc -n $NAMESPACE -l app=kafka
-microk8s kubectl delete pvc -n $NAMESPACE -l app=zookeeper
-microk8s kubectl delete pvc -n $NAMESPACE -l app=kafka-ui
+microk8s kubectl delete pvc -n $NAMESPACE 
 
 # Delete configmaps (if any)
-microk8s kubectl delete configmap -n $NAMESPACE -l app=kafka
-microk8s kubectl delete configmap -n $NAMESPACE -l app=zookeeper
-microk8s kubectl delete configmap -n $NAMESPACE -l app=kafka-ui
+microk8s kubectl delete configmap -n default
+
 
 # Optionally delete all remaining pods, services, and other resources
 microk8s kubectl delete all --all -n $NAMESPACE
